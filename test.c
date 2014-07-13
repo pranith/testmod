d8a4ce17cb47280f8bf2f655d80d76dc3677a089
Pranith Kumar <bobby.prani@gmail.com>
2014-07-13 13:34:54 -0400

    test to see differences between barrier()+atomic_inc+barrier() and atomic_add_return()

--- test.c
+++ test.c
@@ -3,34 +3,23 @@
 
 static int __init test_init(void)
 {
-	char buf[] = "This is a test\nand this should print\neach line separately\nor else";
+	atomic_t i = ATOMIC_INIT(0);
+	pr_info("Loading test module\n");
 
-	char *tmpbuf = buf, c;
+	smp_mb__before_atomic();
+	atomic_inc(&i);
+	smp_mb__after_atomic();
 
-	int used = sizeof(buf), size_pr;
-	int max_iterations = 20, iter = 0;
- 	printk ("My module worked!\n");
-	pr_alert("%d\n", used);
-	while (used > 0 && iter < max_iterations) {
-		iter++;
-		size_pr = strcspn(tmpbuf, "\n");
-		c = tmpbuf[size_pr + 1];
-		tmpbuf[size_pr + 1] = '\0';
-		pr_alert("used: %d\nsize_pr before: %d\n", used, size_pr);
-		size_pr = pr_alert("%s", tmpbuf);
-		pr_alert("size_pr pr_alert: %d\n", size_pr);
-		size_pr = printk(KERN_ALERT "%s", tmpbuf);
-		pr_alert("size_pr printk: %d\n", size_pr);
-		tmpbuf[size_pr + 1] = c;
-		used -= (size_pr + 1);
-		tmpbuf  += size_pr + 1;
-	}
+	atomic_add_return(1, &i);
+	
+	pr_info("val is %d\n", atomic_read(&i));
+
 	return 0;
 }
 
 static void __exit test_exit(void)
 {
- printk ("Unloading my module.\n");
+	pr_info("Unloading test module.\n");
         return;
 }
 
